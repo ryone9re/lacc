8308411fbd0a67f23d975904327c7f698a99992b
ryone9re <ryoichi.kurimoto@gmail.com>
2022-09-28 03:12:10 +0900

    LLVM format

--- file.c
+++ file.c
@@ -1,23 +1,22 @@
 #include "lacc.h"
 
-char *read_file(char *path)
-{
-	FILE *fp = fopen(path, "r");
-	if (!fp)
-		exit_with_error();
+char *read_file(char *path) {
+  FILE *fp = fopen(path, "r");
+  if (!fp)
+    exit_with_error();
 
-	if (fseek(fp, 0, SEEK_END) == -1)
-		exit_with_error();
-	size_t size = ftell(fp);
-	if (fseek(fp, 0, SEEK_SET) == -1)
-		exit_with_error();
+  if (fseek(fp, 0, SEEK_END) == -1)
+    exit_with_error();
+  size_t size = ftell(fp);
+  if (fseek(fp, 0, SEEK_SET) == -1)
+    exit_with_error();
 
-	char *buf = (char *)calloc(1, sizeof(char) * size + 2);
-	fread(buf, size, 1, fp);
+  char *buf = (char *)calloc(1, sizeof(char) * size + 2);
+  fread(buf, size, 1, fp);
 
-	if (size == 0 || buf[size - 1] != '\n')
-		buf[size++] = '\n';
-	buf[size] = '\0';
-	fclose(fp);
-	return (buf);
+  if (size == 0 || buf[size - 1] != '\n')
+    buf[size++] = '\n';
+  buf[size] = '\0';
+  fclose(fp);
+  return (buf);
 }
--- utils.c
+++ utils.c
@@ -1,11 +1,10 @@
 #include "lacc.h"
 
-char *substr(char *str, size_t len)
-{
-	char *s = (char *)calloc(len + 1, sizeof(char));
+char *substr(char *str, size_t len) {
+  char *s = (char *)calloc(len + 1, sizeof(char));
 
-	if (!s)
-		exit_with_error();
-	strncpy(s, str, len);
-	return (s);
+  if (!s)
+    exit_with_error();
+  strncpy(s, str, len);
+  return (s);
 }
